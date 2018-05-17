908be807a411a24e9a34d1561b456ffcc176c60f
Albert Lee <albert.lee@vanderbilt.edu>
2018-05-16 18:01:20 -0700

    Added support for server side rendering

---
+++ server/urls.js
@@ -0,0 +1,6 @@
+module.exports = {
+  Review: {
+    server: 'https://s3.amazonaws.com/starkiller-reviews-static/server-bundle.min.js',
+    client: 'https://s3.amazonaws.com/starkiller-reviews-static/client-bundle.min.js'
+  },
+}
